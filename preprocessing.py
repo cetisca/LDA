60fb9ee11489dd11cebe2f74ba7e72127157f9be
giorkala <giorkala@hotmail.com>
2019-01-10 09:43:16 +0000

    edits

--- preprocessing.py
+++ preprocessing.py
@@ -1,23 +1,24 @@
-import zipfile
-import nltk
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-import re
 import os
 
 FolderToParse = "Data_Part/"
+FolderBOW = "BagsOfWords/"
+# Create a corpus for all documents
+Corpus = []; N=0
 for document in os.listdir( FolderToParse ):
     # load document
     FileToLoad = FolderToParse + document
     # Load text file of wikipedia entry on bird species
-    f = open(FileToLoad,'r')
-    data = f.read()
+    f = open(FileToLoad,'rb')
+    # we ignore non-printable (strange) letters and symbols !
+    text = f.read().decode('ascii', 'ignore')
     f.close()
 
     # Separate words into entries in list, while removing punctuation
     tokenizer = RegexpTokenizer(r'\w+')
-    tokens = tokenizer.tokenize(data)
+    tokens = tokenizer.tokenize(text)
     # make all words lowercase to enable matching for filtering
     words = [token.lower() for token in tokens]
     # remove any stopwords (common words in english language not useful to parse to LDA)
@@ -30,8 +31,29 @@ for document in os.listdir( FolderToParse ):
     # Lemmatization (collapse different forms of the same word into one)
     words = [WordNetLemmatizer().lemmatize(token) for token in words]
 
+    words = set(words) # get rid of duplicates
     # save new files
     tosave = document[:-5] + '_BOW.txt'
-    f = open(BagsOfWords/tosave,'w')
-    f.write(words)
+    f = open(FolderBOW + tosave,'w')
+    for w in words:
+        #ww = str( w+' '.encode("ascii", "replace") )
+        f.write(w + ' ')#.encode("ascii")
+        Corpus.append(w)
     f.close()
+    N += 1
+
+print("Basic preprocessing is done! {0} documents were found.".format(N))
+
+Corpus = set(Corpus)
+f = open("Corpus.txt", 'w')
+for w in Corpus:
+    f.write(w+' ')
+f.close()
+V = len(Corpus)
+print("The vocabulary has size = ", V)
+
+#####
+# Create a term-document matrix
+import numpy as np
+import scipy
+TDM = scipy.sparse.csr_matrix((V,N))
